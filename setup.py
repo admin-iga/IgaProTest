5007d3682bfb2c0a1a35a79d1cc21895fb2a094c
admin-iga <ignatiish@gmail.com>
2024-10-15 06:48:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="IgaProTest",
-    version="0.2.0",
+    version="0.3.0",
     author="IGA-PRO (admin-iga)",
     description="FOR TEST!!!",
     long_description=open("README.md").read(),
